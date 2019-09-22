27aca3132919c4aef6f920d089ce9c46eef6e41c
xxxDNxxx <beerurban@gmail.com>
2019-09-22 17:39:42 +0700

    Add Navigation Lib

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -7,6 +7,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.android.navigation.databinding.FragmentTitleBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -15,9 +17,9 @@ class TitleFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return TextView(activity).apply {
-            setText(R.string.hello_blank_fragment)
-        }
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
+                R.layout.fragment_title,container,false)
+        return binding.root
     }
 
 
